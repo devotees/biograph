a4c67c452a7e087d67baebc94029df0df97829fd
Saul Pwanson <code@saul.pw>
2017-05-11 00:20:24 +0000

    Add weekend events.

--- histo2svg.py
+++ histo2svg.py
@@ -10,6 +10,7 @@ right_grid = 950
 top_grid = 100    # today
 bottom_grid = 900 # birthdate
 
+sqpx_per_hour = .001
 top_date = None
 bottom_date = None
 
@@ -20,6 +21,7 @@ def main():
     timespan('1991-06-20','2017-06-20')
     residence('1991-06-20', '1993-11-20', 'Beograd, Srbija', class_='g9')
     weekday('2016-02-20', '2016-12-31', 9, 21, 'Grad school', class_='o2')
+    weekend('2016-02-20', '2016-04-05', 50, 'Tower of Hanoi', class_='u7')
 
 
 def weekday(start_datestr, end_datestr, start_hour, end_hour, label, **kwargs):
@@ -34,6 +36,15 @@ def weekday(start_datestr, end_datestr, start_hour, end_hour, label, **kwargs):
 def weekday_hour(h):
     x_scale = (right_grid - left_grid)/14
     return left_grid + (h-8)*x_scale
+
+def weekend(start_datestr, end_datestr, nhours, label, **kwargs):
+    y1 = parse_date(start_datestr)
+    y2 = parse_date(end_datestr)
+    x1 = weekday_hour(30)
+    x2 = (y2-y1)/(nhours*sqpx_per_hour) + x1
+    points = [(x1,y1), (x2,y1), (x2,y2), (x1,y2)]
+    d.add(d.polygon(points, **kwargs))
+    d.add(d.text(str(label), x=[x2], y=[y1], stroke='black', style='color:black', **kwargs))
 
 
 def timespan(start_datestr, end_datestr):
