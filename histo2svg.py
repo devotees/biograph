c85af4df52620cbe3650b6cd8a326b935ae46ae2
Saul Pwanson <code@saul.pw>
2017-05-11 01:24:03 +0000

    Changed variable names for clarity.

--- histo2svg.py
+++ histo2svg.py
@@ -30,21 +30,22 @@ def weekday(start_datestr, end_datestr, start_hour, end_hour, label, **kwargs):
     x1 = weekday_hour(start_hour)
     x2 = weekday_hour(end_hour)
     points = [(x1,y1), (x2,y1), (x2,y2), (x1,y2)]
-    d.add(d.polygon(points, **kwargs))
-    d.add(d.text(str(label), x=[x1], y=[y1], stroke='black', style='color:black', **kwargs))
+
+    draw.add(draw.polygon(points, **kwargs))
+    draw.add(draw.text(str(event_label), x=[x1], y=[y1], stroke='black', style='color:black', **kwargs))
 
 def weekday_hour(h):
     x_scale = (right_grid - left_grid)/14
     return left_grid + (h-8)*x_scale
 
-def weekend(start_datestr, end_datestr, nhours, label, **kwargs):
+def weekend(start_datestr, end_datestr, nhours, event_label, **kwargs):
     y1 = parse_date(start_datestr)
     y2 = parse_date(end_datestr)
     x1 = weekday_hour(30)
     x2 = (y2-y1)/(nhours*sqpx_per_hour) + x1
     points = [(x1,y1), (x2,y1), (x2,y2), (x1,y2)]
-    d.add(d.polygon(points, **kwargs))
-    d.add(d.text(str(label), x=[x2], y=[y1], stroke='black', style='color:black', **kwargs))
+    draw.add(draw.polygon(points, **kwargs))
+    draw.add(draw.text(str(event_label), x=[x2], y=[y1], stroke='black', style='color:black', **kwargs))
 
 
 def timespan(start_datestr, end_datestr):
@@ -53,13 +54,14 @@ def timespan(start_datestr, end_datestr):
     bottom_date = dateutil.parser.parse(start_datestr)
     for y in range(bottom_date.year, top_date.year+1):
         dt = parse_date('%s-01-01' % y)
-        d.add(d.line((0, dt), (left_grid, dt), stroke='grey'))
-        d.add(d.text(str(y), x=[0], y=[dt], style='color:black'))
+        draw.add(draw.line((0, dt), (left_grid, dt), stroke='grey'))
+        draw.add(draw.text(str(y), x=[0], y=[dt], style='color:black'))
 
     for h in range(weekday_start_hour, weekday_end_hour+1):
         x = weekday_hour(h)
-        d.add(d.line((x, 0), (x, top_grid), stroke='gray'))
-        d.add(d.text(str(h), x=[x], y=[top_grid], style='color:black'))
+        draw.add(draw.line((x, 0), (x, top_grid), stroke='gray'))
+        draw.add(draw.text(str(h), x=[x], y=[top_grid], style='color:black'))
+
 
 
 def parse_date(datestr):
@@ -74,16 +76,16 @@ def residence(start_date, end_date, address, **kwargs):
     start = parse_date(start_date)
     end = parse_date(end_date)
     points = [(left_grid,start), (right_grid,start), (right_grid,end), (left_grid,end)]
-    d.add(d.polygon(points, **kwargs))
-    d.add(d.text(address, x=[left_grid], y=[start], style='color:black'))
+    draw.add(draw.polygon(points, **kwargs))
+    draw.add(draw.text(address, x=[left_grid], y=[start], style='color:black'))
 
 
 if __name__ == '__main__':
     fnout = sys.argv[1]
 
-    d = svgwrite.Drawing(fnout, preserveAspectRatio='xMidYMid meet')
-    d.add_stylesheet('timeline.css', title='some title')
+    draw = svgwrite.Drawing(fnout, preserveAspectRatio='xMidYMid meet')
+    draw.add_stylesheet('timeline.css', title='some title')
 
     main()
 
-    d.save()
+    draw.save()
